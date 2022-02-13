98ebef95118cb09acbda6e086bae5e4f3ab6d789
verowangari <verowangari34@gmail.com>
2022-02-13 14:25:55 +0300

    create a nagivation bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NnavbarComponent } from './nnavbar/nnavbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NnavbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
