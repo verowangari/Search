0067ee999a9dcaae100d7073f1f2cf10d4baa588
verowangari <verowangari34@gmail.com>
2022-02-14 16:32:07 +0300

    add background color ti the heading

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  access: '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'
+
 };
 
 /*
